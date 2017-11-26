768c7e7e1ca47336df9032fd39aa16ae2037b6d5
mishu <mishu@xtelecom.ro>
2017-11-26 10:23:08 +0200

    fix service provider

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -9,12 +9,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/config' => config_path('enso'),
-        ], 'vue-datatables-config');
+            __DIR__ . '/config' => config_path('enso'),
+        ], 'vuedatatables-config');
 
         $this->publishes([
-            __DIR__.'/resources/assets/js' => resource_path('assets/js'),
-        ], 'vue-datatables-assets');
+            __DIR__ . '/resources/assets/js' => resource_path('assets/js'),
+        ], 'vuedatatables-assets');
     }
 
     public function register()
