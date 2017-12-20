c53186109ae173a88b43af2786aee3b11e6fc092
mishu <mishu@xtelecom.ro>
2017-12-20 09:05:10 +0200

    adds enso tag for assets publishing

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -15,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/resources/assets/js' => resource_path('assets/js'),
         ], 'vuedatatable-assets');
+
+        $this->publishes([
+            __DIR__.'/resources/assets/js' => resource_path('assets/js'),
+        ], 'enso-assets');
     }
 
     public function register()
