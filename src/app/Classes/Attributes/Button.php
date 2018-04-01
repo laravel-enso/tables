6bc6419a531fb4bf4a4bdd2e88bd3f8988273c30
mishu <mishu@xtelecom.ro>
2018-04-01 12:05:58 +0300

    closes #49

--- src/app/Classes/Attributes/Button.php
+++ src/app/Classes/Attributes/Button.php
@@ -6,7 +6,7 @@ class Button
 {
     const Mandatory = ['type', 'icon', 'class'];
 
-    const Optional = ['routeSuffix', 'action', 'fullRoute', 'label', 'method', 'confirmation', 'event', 'message', 'params'];
+    const Optional = ['routeSuffix', 'action', 'fullRoute', 'label', 'method', 'confirmation', 'event', 'message', 'params', 'postEvent'];
 
     const Actions = ['router', 'href', 'ajax', 'export'];
 
