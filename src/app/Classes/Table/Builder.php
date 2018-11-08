bdecca4d688323b9fa539f32c6c2b6d7d4eec774
mishu <mishu@xtelecom.ro>
2018-11-08 08:43:48 +0200

    fixes clear button for typeahead

--- src/app/Classes/Table/Builder.php
+++ src/app/Classes/Table/Builder.php
@@ -138,7 +138,11 @@ class Builder
 
         $this->columns->each(function ($column) {
             if ($column->meta->sortable && $column->meta->sort) {
-                $this->query->orderBy($column->data, $column->meta->sort);
+                true
+                    ? $this->query->orderByRaw(
+                    "ISNULL({$column->data}), {$column->data} {$column->meta->sort}"
+                ) : $this->query->orderBy($column->data, $column->meta->sort);
+                // $this->query->orderBy($column->data, $column->meta->sort);
             }
         });
 
