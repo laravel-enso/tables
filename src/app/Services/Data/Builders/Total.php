8b99cc7934446a3f41262e6ed2900caf9dafba25
mishu <mishu@xtelecom.ro>
2019-10-11 19:01:29 +0300

    v2.3.0

---
+++ src/app/Services/Data/Builders/Total.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace LaravelEnso\Tables\app\Services\Data\Builders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Builder;
+use LaravelEnso\Tables\app\Contracts\Table;
+use LaravelEnso\Tables\app\Contracts\RawTotal;
+use LaravelEnso\Tables\app\Services\Data\Config;
+use LaravelEnso\Tables\app\Exceptions\MetaException;
+
+class Total
+{
+    private $table;
+    private $config;
+    private $query;
+    private $total;
+
+    public function __construct(Table $table, Config $config, Builder $query)
+    {
+        $this->table = $table;
+        $this->config = $config;
+        $this->query = $query;
+        $this->total = [];
+    }
+
+    public function handle()
+    {
+        $this->compute();
+
+        return $this->total;
+    }
+
+    private function compute()
+    {
+        $this->config->columns()
+            ->filter(function ($column) {
+                return $column->get('meta')->get('total')
+                    || $column->get('meta')->get('customTotal')
+                    || $column->get('meta')->get('rawTotal');
+            })->each(function ($column) {
+                $this->total[$column->get('name')] = $column->get('meta')->get('rawTotal')
+                    ? $this->rawTotal($column)
+                    : $this->query->sum($column->get('data'));
+
+                if ($column->get('meta')->get('cents')) {
+                    $this->total[$column->get('name')] /= 100;
+                }
+            });
+    }
+
+    private function rawTotal($column)
+    {
+        if (! $this->table instanceof RawTotal) {
+            throw MetaException::missingInterface();
+        }
+
+        return (clone $this->query)->select(
+            DB::raw("{$this->table->rawTotal($column)} as {$column->get('name')}")
+        )->first()->{$column->get('name')};
+    }
+}
