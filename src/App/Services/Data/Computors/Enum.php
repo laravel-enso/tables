68c9377283ea5e2f64f6c258014220b78fea69cf
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2020-06-04 15:35:14 +0300

    fixes export edge case

--- src/App/Services/Data/Computors/Enum.php
+++ src/App/Services/Data/Computors/Enum.php
@@ -19,8 +19,10 @@ class Enum implements ComputesArrayColumns
     public static function handle(array $row): array
     {
         foreach (self::$columns as $column) {
-            $row[$column->get('name')] = $column
-                ->get('enum')[$row[$column->get('name')]];
+            if ($row[$column->get('name')]) {
+                $row[$column->get('name')] = $column
+                    ->get('enum')[$row[$column->get('name')]];
+            }
         }
 
         return $row;
