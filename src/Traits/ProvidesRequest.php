1660eceeb6e183568163fbc7b68b21363afd6712
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2026-04-15 15:59:53 +0300

    Removes redundant comment, undos data normalization attempt

--- src/Traits/ProvidesRequest.php
+++ src/Traits/ProvidesRequest.php
@@ -11,27 +11,12 @@ trait ProvidesRequest
     public function request(Request $request)
     {
         $aggregator = new FilterAggregator(
-            $this->payload($request->get('internalFilters')),
-            $this->payload($request->get('filters')),
-            $this->payload($request->get('intervals')),
-            $this->payload($request->get('params'))
+            $request->get('internalFilters'),
+            $request->get('filters'),
+            $request->get('intervals'),
+            $request->get('params')
         );
 
-        return new TableRequest(
-            $this->payload($request->get('columns')),
-            $this->payload($request->get('meta')),
-            $aggregator()
-        );
-    }
-
-    private function payload($value)
-    {
-        if (!is_string($value)) {
-            return $value;
-        }
-
-        $decoded = json_decode($value, true);
-
-        return json_last_error() === JSON_ERROR_NONE ? $decoded : $value;
+        return new TableRequest($request->get('columns'), $request->get('meta'), $aggregator());
     }
 }
