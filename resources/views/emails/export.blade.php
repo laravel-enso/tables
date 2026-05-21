19aef59c38a7d60160d5edf111dc40d6e594ee94
Adrian Ocneanu <aocneanu@gmail.com>
2026-05-21 13:38:44 +0300

    Register mail preview in dedicated provider

--- resources/views/emails/export.blade.php
+++ resources/views/emails/export.blade.php
@@ -1,8 +1,16 @@
 @component('mail::message')
+@component('mail::title')
+{{ __('Table export done') }}
+@endcomponent
+
 {{ __('Hi :name', ['name' => $name]) }},
 
 {{ __('You will find the export attached to this email.') }}
 
-{{ __('Thank you') }},<br>
-{{ __(config('app.name')) }}
+@component('mail::file', ['meta' => __(':entries entries', ['entries' => $entries])])
+{{ $filename }}
+@endcomponent
+
+@component('mail::signature')
+@endcomponent
 @endcomponent
---
+++ src/MailServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LaravelEnso\Tables;
+
+use Illuminate\Support\ServiceProvider;
+use LaravelEnso\Mails\Preview\PreviewDefinition;
+use LaravelEnso\Mails\Preview\PreviewRegistry;
+
+class MailServiceProvider extends ServiceProvider
+{
+    public function boot(PreviewRegistry $registry): void
+    {
+        $registry->register(new PreviewDefinition(
+            key: 'table-export-done',
+            name: 'Table Export Done',
+            view: 'laravel-enso/tables::emails.export',
+            data: [
+                'name' => 'Jane',
+                'filename' => 'users-export.xlsx',
+                'entries' => 1284,
+            ],
+            section: PreviewDefinition::Core,
+        ));
+    }
+}
